795e1d90f08cc5153ef209a7aee883a030ad8a05
y00rhighness <90857916+y00rhighness@users.noreply.github.com>
2025-01-18 15:56:08 +0000

    adding variables

---
+++ terransible/backends.tf
@@ -0,0 +1,10 @@
+terraform { 
+  cloud { 
+    
+    organization = "y00rhighness-mtc-terransible" 
+
+    workspaces { 
+      name = "terransible" 
+    } 
+  } 
+}
--- terransible/main.tf
+++ terransible/main.tf
@@ -1,9 +1,21 @@
+resource "random_id" "random" {
+    byte_length = 2
+}
+
 resource "aws_vpc" "mtc_vpc" {
-    cidr_block = "10.123.0.0/16"
+    cidr_block = var.vpc_cidr
     enable_dns_hostnames = true
     enable_dns_support = true
 
     tags = {
-        Name = "mtc_vpc"
+        Name = "mtc_vpc-${random_id.random.dec}"
+    }
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+    vpc_id = aws_vpc.mtc_vpc.id
+
+    tags = {
+        Name = "mtc_igw-${random_id.random.dec}"
     }
 }
