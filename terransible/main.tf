fa2551efdb89603911b689c59cbf22e8114ad93c
y00rhighness <90857916+y00rhighness@users.noreply.github.com>
2025-01-26 21:50:06 +0000

    built AWS security groups

--- terransible/main.tf
+++ terransible/main.tf
@@ -77,3 +77,33 @@ resource "aws_subnet" "mtc_private_subnet" {
     Name = "mtc_private-${count.index + 1}"
   }
 }
+
+resource "aws_route_table_association" "mtc_public_assoc" {
+    count = length(local.azs)
+    subnet_id = aws_subnet.mtc_public_subnet.*.id[count.index]
+    route_table_id = aws_route_table.mtc_public_route.id
+}
+
+resource "aws_security_group" "mtc_sg" {
+  name = "public_sg"
+  description = "Security group for public instances"
+  vpc_id = aws_vpc.mtc_vpc.id
+  }
+
+resource "aws_security_group_rule" "ingress_all" {
+  type = "ingress"
+  from_port = 0
+  to_port = 65535
+  protocol = "-1"
+  cidr_blocks = [var.access_ip]
+  security_group_id = aws_security_group.mtc_sg.id
+}
+
+resource "aws_security_group_rule" "egress_all" {
+  type = "egress"
+  from_port = 0
+  to_port = 65535
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.mtc_sg.id
+}
--- terransible/variables.tf
+++ terransible/variables.tf
@@ -3,12 +3,7 @@ variable "vpc_cidr" {
   default = "10.124.0.0/16"
 }
 
-variable "public_cidrs" {
-  type    = list(string)
-  default = ["10.124.1.0/24", "10.124.3.0/24"]
-}
-
-variable "private_cidrs" {
-  type    = list(string)
-  default = ["10.124.2.0/24", "10.124.4.0/24"]
+variable "access_ip" {
+  type = string
+  default = "72.24.195.218/32"
 }
